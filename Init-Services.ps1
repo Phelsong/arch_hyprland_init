5195e164a5b3f77330eed46e6462c0e7483fa19e
josh wilkinson <joshswilkinson@outlook.com>
2023-10-26 18:21:10 +0000

    Update Init-Services.ps1

--- Init-Services.ps1
+++ Init-Services.ps1
@@ -11,9 +11,10 @@ sudo systemctl enable amdvold
 sudo waydroid init -s GAPPS
 sudo systemctl enable waydroid-container
 Write-Host "# run sudo waydroid shell
-#ANDROID_RUNTIME_ROOT=/apex/com.android.runtime ANDROID_DATA=/data ANDROID_TZDATA_ROOT=/apex/com.android.tzdata ANDROID_I18N_ROOT=/apex/com.android.i18n sqlite3 /data/data/com.google.android.gsf/databases/gservices.db select * from main where name = \ android_id\';'"
+ANDROID_RUNTIME_ROOT=/apex/com.android.runtime ANDROID_DATA=/data ANDROID_TZDATA_ROOT=/apex/com.android.tzdata ANDROID_I18N_ROOT=/apex/com.android.i18n sqlite3 /data/data/com.google.android.gsf/databases/gservices.db select * from main where name = \ android_id\';'"
 "
 Write-Host "# copy android ID"
 Write-Host "# https://www.google.com/android/uncertified"
 sudo waydroid shell 
  
+    
