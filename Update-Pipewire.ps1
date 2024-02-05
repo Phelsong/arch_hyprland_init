c065f1059537ff17928940d88a628c101cda1438
josh wilkinson <joshswilkinson@outlook.com>
2024-02-04 23:26:41 -0600

    misc

--- Update-Pipewire.ps1
+++ Update-Pipewire.ps1
@@ -1,1 +1,3 @@
-yay -Syu "pipewire-git libpipewire-git pipewire-audio-git pipewire-pulse-git pipewire-alsa-git pipewire-jack-git pipewire-v4l2-git pipewire-ffado-git pipewire-zeroconf-git pipewire-x11-bell-git libcamera-git libcamera-ipa-git"
+yay -S pipewire-audio-git pipewire-pulse-git pipewire-alsa-git pipewire-jack-git pipewire-v4l2-git pipewire-ffado-git pipewire-zeroconf-git pipewire-x11-bell-git libcamera libcamera-ipa
+#yay -S "pipewire-git lib-pipewire-git pipewire-audio-git pipewire-pulse-git pipewire-alsa-git pipewire-jack-git pipewire-v4l2-git pipewire-ffado-git pipewire-zeroconf-git pipewire-x11-bell-git libcamera libcamera-ipa"
+
